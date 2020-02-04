0b0beca0dbbb62b6d6c5745dae72b0198e75b764
diankra <56646341+diankra@users.noreply.github.com>
2020-02-04 14:48:16 +0100

    Navegación básica
    Páginas vacías

--- portal_de_rol/src/main/java/es/urjc/code/WebController.java
+++ portal_de_rol/src/main/java/es/urjc/code/WebController.java
@@ -10,16 +10,48 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class WebController {
 	
 	@GetMapping("/foro")
-	public String greeting(Model model) {
+	public String foro(Model model) {
 		
 	return "foro_general";
 	}
 	
 	@GetMapping("/partidas_publicas")
-	public String saludo_partidas(Model model) {
+	public String partidas(Model model) {
 		
 	return "partidas_publicas";
 	}
+	
+	@GetMapping("/crear_usuario")
+	public String crear_usuario(Model model) {
+		
+	return "crear_usuario";
+	}
 
+	@GetMapping("/inicia_sesion")
+	public String inicia_sesion(Model model) {
+		
+	return "inicia_sesion";
+	}
+	
+	@GetMapping("/crear_partida")
+	public String crear_partida(Model model) {
+		
+	return "crear_partida";
+	}
+	
+	@GetMapping("/partidas_privadas")
+	public String partidas_privadas(Model model) {
+		
+	return "partidas_privadas";
+	}
+	
+	@GetMapping("/crear_ficha")
+	public String fichas(Model model) {
+		
+	return "crear_ficha";
+	}
+	
+	
 }
+
 
