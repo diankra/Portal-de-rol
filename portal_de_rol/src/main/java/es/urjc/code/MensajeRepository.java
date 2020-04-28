bbb975c5eba2022ece6387c02bf9a03ee791fb3f
PandaJavi <javi98itaca@gmail.com>
2020-04-28 13:46:56 +0200

    Update MensajeRepository.java

--- portal_de_rol/src/main/java/es/urjc/code/MensajeRepository.java
+++ portal_de_rol/src/main/java/es/urjc/code/MensajeRepository.java
@@ -2,8 +2,17 @@ package es.urjc.code;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@CacheConfig(cacheNames="portalderol")
 public interface MensajeRepository extends JpaRepository<Mensaje, Long> {
+	
+	@SuppressWarnings("unchecked")
+	@CacheEvict(allEntries=true)
+	Mensaje save (Mensaje entity);
+	@Cacheable
 	List<Mensaje> findMensajesByAutor(Usuario user);
 	}
